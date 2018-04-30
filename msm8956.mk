e78416a7459b5aa550b12061e7a057877bb9a27f
TheScarastic <warabhishek@gmail.com>
2018-04-30 13:53:02 +0530

    msm8956-common: use binderized aw2013 hal and livedisplay hal

--- msm8956.mk
+++ msm8956.mk
@@ -154,7 +154,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0_vendor
 
 # DRM
-+PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
@@ -235,9 +235,11 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8952
+    android.hardware.light@2.0-service.aw2013
+
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
