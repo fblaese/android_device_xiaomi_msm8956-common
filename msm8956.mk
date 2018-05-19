9fa69633d8f12e5616a238ee5a741edee1bbf12b
TheStrechh <TheStrechh@gmail.com>
2018-05-19 14:52:13 +0000

    msm8956-common: Set shipping API level via inherited makefile

--- msm8956.mk
+++ msm8956.mk
@@ -21,6 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/msm8956-common/msm8956-common-vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
